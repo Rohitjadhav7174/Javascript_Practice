64518b6ca08b220b87ac1f2c0af489485bd8a6d5
Rohit Jadhav <113785547+Rohitjadhav7174@users.noreply.github.com>
2024-08-25 09:02:24 +0000

    new files added

---
+++ DataType/math.js
@@ -0,0 +1,13 @@
+
+
+// const restaurant = "Chipotle";
+
+// console.log(`My favorite restaurant is ${restaurant}!`);
+
+
+let number =120.234;
+console.log(number.toString().length);
+
+console.log(number.toFixed(2));
+
+console.log(number.toPrecision(3));
---
+++ DataType/stackheap.js
@@ -0,0 +1,26 @@
+//stack
+
+let name ="ROhit Jadhav"
+
+let name2=name
+
+name2 = "Aditya Jadhav"
+
+// console.log(name);
+// console.log(name2);
+
+
+//heap
+
+let user1={
+    name:"rohit jadhav",
+    email :"rj@gmail.com"
+}
+
+let user2=user1
+
+user2.name="aditya jadhav"
+
+console.log(user1);
+console.log(user2);
+
