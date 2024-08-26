aee7aac9f154073285d8524f12960daa6c29161b
Rohit Jadhav <113785547+Rohitjadhav7174@users.noreply.github.com>
2024-08-26 17:38:11 +0000

    date

---
+++ datatime/datetime.js
@@ -0,0 +1,19 @@
+let mydate =new Date();
+// console.log(mydate);
+
+// console.log(mydate.toDateString());
+
+// console.log(mydate.toISOString());
+
+// console.log(mydate.toJSON());
+
+
+// timestamp
+
+let create =Date.now();
+console.log(create);
+
+
+console.log(mydate.getTime())
+
+
